0764b5ca13e69bc2e1e8f73ab961f46714da9544
Ayllah <ayllah_ahmad@hotmail.com>
2018-05-27 15:53:34 -0300

    Bug Ordenação | testar Excluir

--- include/Lista.h
+++ include/Lista.h
@@ -3,14 +3,18 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 //Estrutura para armazenar e tratar as chaves primarias
 
 struct ChavesPrimarias {
-	 std::string chavePrim; 
-			unsigned RRN;
-			char MarcadorAtivo;
-			ChavesPrimarias *prox;
+	std::string chave; 
+	std::string curso;
+	unsigned indice;
+	unsigned RRN;
+	char MarcadorAtivo;
+	ChavesPrimarias *prox;
+	ChavesPrimarias *anterior;
 };
 
 class ListaPrimaria{
@@ -24,8 +28,9 @@ public:
 //nos da lista de chave primaria
 struct ChavesSecundarias {
 	std::string curso;
-	unsigned referencia;
+	ChavesPrimarias* chavePrimaria;
 	ChavesSecundarias* prox;
+	ChavesSecundarias* anterior;
 };
 
 //Estrutura para armazenar e tratar as listas invertidas
@@ -57,10 +62,25 @@ class Lista {
 */
 class Arquivos{
 public:
-	void ArquivoPrimario();
-	void InserirPrimario(ChavesPrimarias **no, std::string chave, unsigned RRN, std::ostream& indicelistaP);
-	void InserirSecundario(ChavesSecundarias **no, std::string curso, unsigned RRN, std::ostream& indicelistaS);	
-	void LerListaOriginal(std::istream& Lista, std::ostream& indicelistaP, std::ostream& indicelistaS, ChavesPrimarias **noP, ChavesSecundarias **noS);
+	int TamanhoListaPrim(ChavesPrimarias *Inicio);
+	bool ExcluirRegistroP(ChavesPrimarias **no, std::string chave, std::ostream& indicelistaP);
+	void Excluir(ListaPrimaria *listaPrimarios1, std::ostream& indicelistaP1);
+
+	void ArquivoPrimSec(ListaPrimaria *listaPrimarios1, ListaPrimaria* listaPrimarios2, ListaInvertida* listaInvertida1, ListaInvertida* listaInvertida2);
+	void LerListaOriginal(ListaInvertida* listaInvertida, ListaPrimaria* listaPrimaria, unsigned *indice, std::istream& Lista, std::ostream& indicelistaP, std::ostream& indicelistaS, ChavesPrimarias **noP, ChavesSecundarias **noS, ChavesSecundarias *anteriorS, ChavesPrimarias *anteriorP);
+	
+	void InserirPrimario(unsigned *indice, ChavesPrimarias **no, ChavesPrimarias *anterior, std::string chave, unsigned RRN, std::string curso, std::ostream& indicelistaP);
+	void InserirSecundario(ChavesSecundarias **no, ChavesSecundarias *anterior, std::string curso, ChavesPrimarias **chavePrimaria, std::ostream& indicelistaS);
+	bool existeCurso(std:: string curso, ListaInvertida *lista);
+	void Insere_proxOrdem(ListaPrimaria* lista, ChavesPrimarias** no, std::string curso);
+
+	void TrocaNoSec(ChavesSecundarias *atual, ChavesSecundarias *proximo);
+	void OrdenaListaSecundaria(ListaInvertida *lista);
+	void TrocaNoPrim(ChavesPrimarias **atual, ChavesPrimarias **proximo);
+	void OrdenaListaPrimaria(ListaPrimaria **lista, ListaInvertida** invertida);
+
+	void ImprimeSecundaria(ListaInvertida* lista);
+	void ImprimePrimaria(ListaPrimaria* lista);
 };
 
  
---
+++ include/heapsort.h
@@ -0,0 +1,8 @@
+#ifndef HEAPSORT_H					
+#define HEAPSORT_H
+#include "Lista.h"
+
+void heapSort(ChavesSecundarias *tmp);
+void swap(int *a,int *b);
+
+#endif //HEAPSORT_H
